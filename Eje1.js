4bc4ad0186b8252c510909adc29f9624e7540634
XimenaRubio212 <ximenarubio121@gmail.com>
2025-12-15 14:03:02 -0500

    Se realizo documentacion en el ejercicio 1 de apropiacion

--- Eje1.js
+++ Eje1.js
@@ -1,7 +1,10 @@
+//Se muestra el comportamiento sincrono en consola
 console.log("Inicio");
-
+//funcion que simula una operacion asincrona con setTimeout
 setTimeout(() => {
+    //mensaje que se muestra en consola despues de 2 segundos
     console.log("Operacion finalizada por completo...");
+    //espera de tiempo para simular una operacion asincrona
 }, 2000);
-
+//este mensaje se muestra en consola y su comportamiento es sincrono
 console.log("Fin");
