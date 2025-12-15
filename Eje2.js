01ad5f4ce96f9ff21a7ab1494f5d4094391c46f1
XimenaRubio212 <ximenarubio121@gmail.com>
2025-12-15 14:08:51 -0500

    Se realizo la documentacion del ejericio 3 de apropiacion

--- Eje2.js
+++ Eje2.js
@@ -1,9 +1,11 @@
+//se muestra el mensaje antes del ciclo del comportamiento sincronico
 console.log("Antes del ciclo pesado");
-
-// Simulamos trabajo síncrono intensivo (~3s en muchos entornos)
+// Simulamos trabajo síncrono
 const inicio = Date.now();
+// ciclo que dura aproximadamente 3 segundos
 while (Date.now() - inicio < 3000) {
   // bloquea el hilo
 }
-
+//este mensaje se muestra despues de que el ciclo pesado ha terminado
+//del comportamiento sincronico
 console.log("Después del ciclo pesado — todo se detuvo durante 3s");
