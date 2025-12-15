8abb3896baeeaf1732d20112309cb9c04a7a989d
XimenaRubio212 <ximenarubio121@gmail.com>
2025-12-15 13:34:05 -0500

    Se realizo el eje6.js de la actividad de apropiacion

---
+++ Eje6.js
@@ -0,0 +1,14 @@
+console.log("Intentando operación riesgosa...");
+
+new Promise((resolve, reject) => {
+  const exito = Math.random() > 0.5; // 50% de éxito
+  setTimeout(() => {
+    if (exito) {
+      resolve("✅ Éxito");
+    } else {
+      reject("❌ Falló");
+    }
+  }, 1000);
+})
+.then(mensaje => console.log(mensaje))
+.catch(error => console.log(error));
