1fbb9b1163405d941e26fee4fb8fe223e579b95a
XimenaRubio212 <ximenarubio121@gmail.com>
2025-12-15 14:16:28 -0500

    Se realiza el eje3 y ejer4 de la actividad de apropiacion

--- Eje3.js
+++ Eje3.js
@@ -1,6 +1,9 @@
-//
+//este mensaje se muesta antes de iniciar el comportamiento asincronico
+//este mensaje es sincronico
 console.log("Iniciando pedido...");
-
+//comportamiento asincronico con setTimeout
 setTimeout(() => {
+  //este es el mensjae asincrona que se muesta
+  //despues de 3 segundos
   console.log("Pedido entregado");
 }, 3000);
--- Eje4.js
+++ Eje4.js
@@ -1,10 +1,14 @@
+//este mensaje sincrono se muestra primero
 console.log("Tomando datos...");
-
+//desúes se realiza la funcion asíncrona
 setTimeout(() => {
+  //este mensaje asíncrono se muestra después de 1 segundo
   console.log("Procesando datos...");
-
+  //se realiza la segunda funcion asíncrona de 1 segundo
   setTimeout(() => {
+    //continuando con este mensaje sincrono que se muestra después de otro segundo
     console.log("Mostrando resultado");
   }, 1000);
-
+//este es el del primer setTimeout para
+//que muestre en consola la primera función asíncrona después de 1 segundo
 }, 1000);
