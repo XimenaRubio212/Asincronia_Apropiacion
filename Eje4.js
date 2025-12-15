252037037ed145a41467e7676fae4215baa62f6d
XimenaRubio212 <ximenarubio121@gmail.com>
2025-12-15 13:30:55 -0500

    Se realizo el ejericio 4 de la actividad de apropiacion

---
+++ Eje4.js
@@ -0,0 +1,10 @@
+console.log("1. Tomando datos...");
+
+setTimeout(() => {
+  console.log("2. Procesando datos...");
+
+  setTimeout(() => {
+    console.log("3. Mostrando resultado");
+  }, 1000);
+
+}, 1000);
